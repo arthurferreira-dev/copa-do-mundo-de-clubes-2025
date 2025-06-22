4b07c819038b8ce84f6a2515e0a4a7f0c7667497
Arthur Ferreira <tutudacosta996@gmail.com>
2025-06-21 23:18:00 -0300

    start

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagens/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" sizes="192x192" href="imagens/favicon/android-chrome-192x192.png">
+    <link rel="icon" sizes="512x512" href="imagens/favicon/android-chrome-512x512.png">
+    <link rel="apple touch icon" href="imagens/favicon/apple-touch-icon.png">
+    <link rel="icon" sizes="16x16" href="imagens/favicon/favicon-16x16.png" type="image/x-png">
+    <link rel="icon" sizes="32x32" href="imagens/favicon/favicon-32x32.png" type="image/x-png">
+    <title>Copa do Mundo de Clubes 2025 - Arthur Ferreira Dev</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1 id="title">
+            Copa do Mundo de Clubes 2025
+        </h1>
+        <img src="imagens/fifa-logo.png" alt="Fifa Logo" id="fifa">
+    </header>
+    <main>
+        <article>
+            <?php
+            # Tudo em ordem alfabética:
+
+                # Grupo A
+                $ahly = 'Al Ahly';
+                $miami = 'Inter Miami';
+                $palmeiras = 'Palmeiras';
+                $porto = 'Porto';
+
+                # Grupo B
+                $madrid = 'Atlético de Madrid';
+                $botafogo = 'Botafogo';
+                $psg = 'Paris Saint-Germain';
+                $seattle = 'Seattle Sounders';
+
+                # Grupo C
+                $auckland = 'Auckland City';
+                $bayern = 'Bayern de Munique';
+                $benfica = 'Benfica';
+                $boca = 'Boca Juniors';
+
+                # Grupo D
+                $chelsea = 'Chelsea';
+                $esperance = 'Espérance';
+                $angeles = 'Los Angeles FC';
+                $flamengo = 'Flamengo';
+
+                # Grupo E
+                $inter = 'Internazionale';
+                $monte = 'Monterrey';
+                $river = 'River Plate';
+                $urawa = 'Urawa Reds';
+
+                # Grupo F
+                $borussia = 'Borussia Dortmund';
+                $fluminense = 'Fluminense';
+                $mamelodi = 'Mamelodi Sundowns';
+                $kpop = 'Ulsan HD';
+
+                # Grupo G
+                $ain = 'Al-Ain';
+                $juventus = 'Juventus';
+                $city = 'Manchester City';
+                $casablanca = 'Wydad Casablanca';
+
+                # Grupo H
+                $hilal = 'Al-Hilal';
+                $pachuca = 'Pachuca';
+                $salzburg = 'RB Salzburg';
+                $real = 'Real Madrid';
+
+                # Nacionalidades dos times:
+                $br = 'Brasil';
+                $es = 'Espanhol';
+                $pt = 'Português';
+                $es = 'Estadunidense';
+                $eg = 'Egito';
+                $fra = 'França';
+                $ger = 'Alemanha';
+                $arg = 'Argentina';
+                $nzl = 'Nova Zelândia';
+                $tun = 'Tunísia';
+                $jp = 'Japão';
+                $mx = 'México';
+                $co = 'Coreia (do Sul)';
+                $afs = 'África do Sul';
+                $eng = 'Inglaterra';
+                $mar = 'Marrocos';
+                $ita = 'Itália';
+                $aus = 'Austría';
+                $eau = 'Emirados Árabes Unidos';
+                $ars = 'Árabia Saudita';
+            ?>
+        </article>
+    </main>
+</body>
+</html>
